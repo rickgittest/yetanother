89a4deed578c272bab9014cab4cc5de419ca0ea7
Rick Horowitz <rickhoro@gmail.com>
2012-12-07 19:03:14 -0500

    Initial commit.

---
+++ YetAnother/src/YetAnotherClass.java
@@ -0,0 +1,12 @@
+
+public class YetAnotherClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is yet another class");
+	}
+
+}
